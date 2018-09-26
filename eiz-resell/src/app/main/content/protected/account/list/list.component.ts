bdab11540b491a4d80a5d9064ea720bc30f21754
Lei Zhang <LeiZhang@LeideMBP.gateway>
2018-09-26 11:37:51 +1000

    finish list page table

--- eiz-resell/src/app/main/content/protected/account/list/list.component.ts
+++ eiz-resell/src/app/main/content/protected/account/list/list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {SelectionModel} from '@angular/cdk/collections';
 
 @Component({
@@ -7,8 +7,12 @@ import {SelectionModel} from '@angular/cdk/collections';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
-    displayedColumns: string[] = ['id', 'courier', 'createdate', 'consignmentnum', 'action'];
+    @ViewChild('chooseFile') chooseFile: ElementRef;
+    @ViewChild('fileName') fileName: ElementRef;
+
+    displayedColumns: string[] = ['id', 'index', 'email', 'action'];
     dataSource = ELEMENT_DATA;
+
     selection = new SelectionModel<PeriodicElement>(true, []);
 
     constructor() {
@@ -16,35 +20,17 @@ export class ListComponent implements OnInit {
 
     ngOnInit() {
     }
-
-    isAllSelected() {
-        const numSelected = this.selection.selected.length;
-        const numRows = this.dataSource.length;
-        return numSelected === numRows;
-    }
-
-    /** Selects all rows if they are not all selected; otherwise clear selection. */
-    masterToggle() {
-        this.isAllSelected() ?
-            this.selection.clear() :
-            this.dataSource.forEach(row => this.selection.select(row));
-    }
-
 }
 
 export interface PeriodicElement {
     id: number;
-    courier: number;
-    createdate: string;
-    consignmentnum: number;
+    email: string;
 }
 
 const ELEMENT_DATA: PeriodicElement[] = [
-    {id: 1, courier: 1, createdate: '2018-09-12 14:35:45', consignmentnum: 12},
-    {id: 1, courier: 2, createdate: '2018-09-11 14:35:45', consignmentnum: 16},
-    {id: 1, courier: 1, createdate: '2018-09-12 14:35:45', consignmentnum: 2},
-    {id: 1, courier: 2, createdate: '2018-09-04 14:35:45', consignmentnum: 7},
-    {id: 1, courier: 1, createdate: '2018-09-22 14:35:45', consignmentnum: 9},
-    {id: 1, courier: 2, createdate: '2018-09-26 14:35:45', consignmentnum: 26},
-    {id: 1, courier: 1, createdate: '2018-09-12 14:35:45', consignmentnum: 32},
+    {id: 1, email: '123@gmail.com'},
+    {id: 2, email: '123@gmail.com'},
+    {id: 3, email: '123@gmail.com'},
+    {id: 4, email: '123@gmail.com'},
+    {id: 5, email: '123@gmail.com'},
 ];
